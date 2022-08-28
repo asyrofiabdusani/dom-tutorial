92f5eab7b7c51b176cd067ff6b0a8906658feb41
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-08-28 23:06:15 +0700

    manipulate element in the HTML file using JS file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const title = document.querySelector('.title');
+const container = document.querySelector('.container');
+const content = document.querySelector('.content');
+const txtContent = content.textContent;
+
+title.style.textAlign = 'center';
+title.style.backgroundColor = 'salmon';
+container.style.backgroundColor = 'salmon';
+content.classList.add('anim');
